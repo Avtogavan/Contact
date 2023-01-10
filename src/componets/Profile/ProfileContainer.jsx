63d27b29030fbed063ac5616db854912cc21d162
Петр Кенн <kniv132@gmail.com>
2023-01-10 16:39:59 +0700

    u

--- src/componets/Profile/ProfileContainer.jsx
+++ src/componets/Profile/ProfileContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { CardsData } from '../CardsData'
 import Profile from './Profile'
 import s from '../Cards/Cards.module.sass'
@@ -7,31 +7,22 @@ import { useParams } from 'react-router-dom'
 export default function ProfileContainer({param}) {
 
     let uId = useParams()
-//     // console.log(uId.id);
-// useEffect(()=> {
-//   let userId = uId.id
-//         if (!userId) {
-//             userId = param        
-//         }
-//     //  props.setParam(uId.id)
-// })    
+
   return (
-    <>
-      { CardsData.filter(e=> uId.id !== undefined ? e.id == uId.id : e.id === param).map((e, i) => 
-                                <div className={s.card__width} key={i}> 
-                                    
-                                
-                              
-                                    <Profile
-                                     namePerson={e.namePerson} 
-                                     position={e.position} 
-                                     image={e.image} 
-                                     inst={e.inst} 
-                                     phone={e.phone}
-                                    />
-                                  
-                                </div> 
-                              )}
-    </>
-  )
+            <>
+                { CardsData.filter(e=> uId.id !== undefined ? e.id == uId.id : e.id === param).map((e, i) => 
+                    <div className={s.card__width} key={i}> 
+                    
+                        <Profile
+                            namePerson={e.namePerson} 
+                            position={e.position} 
+                            image={e.image} 
+                            inst={e.inst} 
+                            phone={e.phone}
+                        />
+                        
+                    </div> 
+                )}
+            </>
+        )
 }
